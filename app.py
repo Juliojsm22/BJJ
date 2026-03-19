89a7939a8ae24ccd75b34adbbc91e9061f79a7b8
Juliojsm22 <juliosobalvarro10@gmail.com>
2026-03-19 09:59:20 -0600

    de3

--- app.py
+++ app.py
@@ -202,4 +202,4 @@ def clientes_pdf():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # usa el puerto de Render
     app.run(host="0.0.0.0", port=port, debug=True)
-    A
+    
