30a37014ead2031c7dab446c2d76add9d169bd83
Juliojsm22 <juliosobalvarro10@gmail.com>
2026-03-19 17:21:31 -0600

    change

--- app.py
+++ app.py
@@ -201,5 +201,5 @@ def clientes_pdf():
 # 🚀 RUN
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # usa el puerto de Render
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
     
